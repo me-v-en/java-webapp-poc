b5580e455580fee63cdaee87582aac1aa6f61555
Meven <meven.ameline@gmail.com>
2024-08-22 22:08:53 +0200

    Added create employee form and controller handling

--- src/main/java/com/example/meven/webapp/controller/EmployeeController.java
+++ src/main/java/com/example/meven/webapp/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,6 +24,8 @@ public class EmployeeController {
     public String home(Model model) {
         Iterable<Employee> employees = employeeService.getAllEmployees();
         model.addAttribute("employees", employees);
+        Employee e = new Employee();
+        model.addAttribute("employee", e);
         // Return the content of the home.html file
         return "home";
     }
@@ -30,6 +34,12 @@ public class EmployeeController {
     public ModelAndView deleteEmployee(@PathVariable("id") final int id) {
         employeeService.deleteEmployee(id);
         // redirect to the home
+        return new ModelAndView("redirect:/");
+    }
+
+    @PostMapping("/saveEmployee")
+    public ModelAndView saveEmployee(@ModelAttribute Employee employee) {
+        employeeService.saveEmployee(employee);
         return new ModelAndView("redirect:/");
     }
 }
